1383b638f4831740d793ead45fc1777025a51fb8
PriyankaRumal <priyankarumal182000@gmail.com>
2022-10-04 19:01:06 +0530

    Added day 7 assign

---
+++ ArraySum.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+a=(3 -2 -1)
+sum=0
+for i in ${a[@]}
+do
+	sum=`expr $sum + $i`
+done
+
+echo $sum
---
+++ Dublicate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+#count=0
+#rev=0
+#for ((i=1; i<=10; i++))
+#{
+#	while (($i>0))
+#	do
+#	rem=$(($i%10))
+#	rev=$((($rem*10) + $rev))
+#	i=`expr $i / 10`
+#	done
+#	if (( $rev == $i ))
+#	then
+#		arr[((count++))]=$i
+#		((count++))
+#	else
+#		echo "do"
+#	fi
+#}
+declare -a a
+a=($(seq 1 100))
+echo "${a[$i]" is "$i"
+while (( $i == 11 ))
+do
+if (( $i == 11 ))
+then
+	echo $i;
+fi
+done
+
+
+
+
